0d8c6cb159a3c0c52de08b4a9e897e5aecaef083
emiyl <me@emiyl.com>
2021-11-18 05:01:36 +0000

    full table

--- docs/.vuepress/components/fullTable.vue
+++ docs/.vuepress/components/fullTable.vue
@@ -1,17 +1,18 @@
 <template>
-  <h2 v-text="deviceList[device || d].name"/>
   <table>
+    <thead>
+    </thead>
     <tbody>
       <tr>
         <th>Version</th>
         <th>Build</th>
-        <th>Released</th>
+        <th>Devices</th>
       </tr>
         <template v-for="majFw in iosVer"><template v-for="midFw in majFw"><template v-for="minFw in midFw"><template v-for="v in minFw">
-          <tr v-if="v.devices.includes(device || d)">
+          <tr>
             <td v-text="v.ver"/>
             <td v-text="v.build"/>
-            <td v-text="v.released"/>
+            <td><template v-for="d in v.devices"><a :href="d">{{getDeviceName(d)}}<template v-if="v.devices.indexOf(d) < v.devices.length - 1">, </template></a></template></td>
           </tr>
         </template></template></template></template>
     </tbody>
@@ -20,7 +21,7 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { usePageData, usePageFrontmatter } from '@vuepress/client'
+import { usePageData } from '@vuepress/client'
 const page = usePageData()
 
 var d = page.value.path;
@@ -29,8 +30,14 @@ d = d[d.length-1]
 d = d.split('.')
 d = d[d.length-2]
 
-const deviceList = computed(() => require('./json/deviceList'));
+var deviceList = require('./json/deviceList');
 const iosVer = computed(() => { return {"0": require('./json/ios/1'), "1": require('./json/ios/2'),"2": require('./json/ios/3'),"3": require('./json/ios/4'),"4": require('./json/ios/5'),"5": require('./json/ios/6'),"6": require('./json/ios/7'),"7": require('./json/ios/8'),"8": require('./json/ios/9'),"9": require('./json/ios/10'),"10": require('./json/ios/11'),"11": require('./json/ios/12'),"12": require('./json/ios/13'),"13": require('./json/ios/14'),"14": require('./json/ios/15'),}});
+
+function getDeviceName(d) {
+  if (deviceList.hasOwnProperty(d))
+    return deviceList[d].name;
+  return '-1';
+}
 
 const props = defineProps({
   device: { type: String, },
