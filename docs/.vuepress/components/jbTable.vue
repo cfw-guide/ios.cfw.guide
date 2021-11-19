d7d71284097b684e09c5508ae92c7f376aa3b25d
emiyl <me@emiyl.com>
2021-11-19 10:43:15 +0000

    added info fields to jb pages

--- docs/.vuepress/components/jbTable.vue
+++ docs/.vuepress/components/jbTable.vue
@@ -1,4 +1,8 @@
 <template>
+  <template v-if="jbList[jb].info">
+    <h2>Info</h2>
+    <p><template v-for="info in jbList[jb].info">{{ info.name }}: <a v-if="info.url" :href="info.url" :target="(info.external) ? '_blank' : '_self'">{{ info.value }}</a><template v-if="!info.url">{{ info.value }}</template><svg v-if="info.external" class="icon outbound" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg><br></template></p>
+  </template>
   <h2>Compatibility Listing</h2>
   <template v-for="compatList in fwLists">
     <h3>Devices</h3>
