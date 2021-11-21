7038a2d953130b206b996edda03a455dfeaa7c1c
Converseallstar95 <59682978+Converseallstar95@users.noreply.github.com>
2021-11-21 15:08:34 -0400

    Update deviceList.js
    Renamed iPad and iPod touch devices according to Apple Support identification articles

--- docs/.vuepress/json/deviceList.js
+++ docs/.vuepress/json/deviceList.js
@@ -1,6 +1,6 @@
 module.exports = {
   "iPhone1,1": {
-    "name"  : "iPhone 2G",
+    "name"  : "iPhone",
     "soc"   : "APL0098",
     "arch"  : "32-bit",
     "model"         : ["A1203"],
@@ -32,7 +32,7 @@ module.exports = {
     "discontinued"  : "2013-09-10"
   },
   "iPhone3,2": {
-    "name"  : "iPhone 4 (GSM / 2012)",
+    "name"  : "iPhone 4 (GSM, 2012)",
     "soc"   : "A4",
     "arch"  : "32-bit",
     "discontinued"  : "2013-09-10"
@@ -70,7 +70,7 @@ module.exports = {
     "discontinued"  : "2013-09-10"
   },
   "iPhone5,3": {
-    "name"  : "iPhone 5C (GSM)",
+    "name"  : "iPhone 5c (GSM)",
     "soc"   : "A6",
     "arch"  : "32-bit",
     "model"         : ["A1456", "A1507", "A1516", "A1529", "A1532"],
@@ -78,7 +78,7 @@ module.exports = {
     "discontinued"  : "2015-09-09"
   },
   "iPhone5,4": {
-    "name"  : "iPhone 5C (Global)",
+    "name"  : "iPhone 5c (Global)",
     "soc"   : "A6",
     "arch"  : "32-bit",
     "model"         : ["A1456", "A1507", "A1516", "A1529", "A1532"],
@@ -86,7 +86,7 @@ module.exports = {
     "discontinued"  : "2015-09-09"
   },
   "iPhone6,1": {
-    "name"  : "iPhone 5S (GSM)",
+    "name"  : "iPhone 5s (GSM)",
     "soc"   : "A7",
     "arch"  : "64-bit",
     "model"         : ["A1457", "A1518", "A1528", "A1530", "A1533"],
@@ -94,7 +94,7 @@ module.exports = {
     "discontinued"  : "2016-03-21"
   },
   "iPhone6,2": {
-    "name"  : "iPhone 5S (Global)",
+    "name"  : "iPhone 5s (Global)",
     "soc"   : "A7",
     "arch"  : "64-bit",
     "model"         : ["A1453"],
@@ -102,7 +102,7 @@ module.exports = {
     "discontinued"  : "2016-03-21"
   },
   "iPhone7,1": {
-    "name"  : "iPhone 6+",
+    "name"  : "iPhone 6 Plus",
     "soc"   : "A8",
     "arch"  : "64-bit",
     "model"         : ["A1522", "A1524", "A1593"],
@@ -118,7 +118,7 @@ module.exports = {
     "discontinued"  : "2018-09-12"
   },
   "iPhone8,1": {
-    "name"  : "iPhone 6S",
+    "name"  : "iPhone 6s",
     "soc"   : "A9",
     "arch"  : "64-bit",
     "model"         : ["A1633", "A1688", "A1700"],
@@ -126,7 +126,7 @@ module.exports = {
     "discontinued"  : "2018-09-12"
   },
   "iPhone8,2": {
-    "name"  : "iPhone 6S+",
+    "name"  : "iPhone 6s Plus",
     "soc"   : "A9",
     "arch"  : "64-bit",
     "model"         : ["A1634", "A1687", "A1699"],
@@ -134,7 +134,7 @@ module.exports = {
     "discontinued"  : "2018-09-12"
   },
   "iPhone8,4": {
-    "name"  : "iPhone SE",
+    "name"  : "iPhone SE (1st generation)",
     "soc"   : "A9",
     "arch"  : "64-bit",
     "model"         : ["A1662", "A1723", "A1724"],
@@ -147,7 +147,7 @@ module.exports = {
     "arch"  : "64-bit"
   },
   "iPhone9,2": {
-    "name"  : "iPhone 7+ (Global)",
+    "name"  : "iPhone 7 Plus (Global)",
     "soc"   : "A10",
     "arch"  : "64-bit"
   },
@@ -157,7 +157,7 @@ module.exports = {
     "arch"  : "64-bit"
   },
   "iPhone9,4": {
-    "name"  : "iPhone 7+ (GSM)",
+    "name"  : "iPhone 7 Plus (GSM)",
     "soc"   : "A10",
     "arch"  : "64-bit"
   },
@@ -167,7 +167,7 @@ module.exports = {
     "arch"  : "64-bit"
   },
   "iPhone10,2": {
-    "name"  : "iPhone 8+ (Global)",
+    "name"  : "iPhone 8 Plus (Global)",
     "soc"   : "A11",
     "arch"  : "64-bit"
   },
@@ -182,7 +182,7 @@ module.exports = {
     "arch"  : "64-bit"
   },
   "iPhone10,5": {
-    "name"  : "iPhone 8+ (GSM)",
+    "name"  : "iPhone 8 Plus (GSM)",
     "soc"   : "A11",
     "arch"  : "64-bit"
   },
@@ -227,7 +227,7 @@ module.exports = {
     "arch"  : "64-bit"
   },
   "iPhone12,8": {
-    "name"  : "iPhone SE (2020)",
+    "name"  : "iPhone SE (2nd generation)",
     "soc"   : "A13",
     "arch"  : "64-bit"
   },
@@ -272,402 +272,402 @@ module.exports = {
     "arch"  : "64-bit"
   },
   "iPad1,1": {
-    "name"  : "iPad 1",
+    "name"  : "iPad",
     "soc"   : "A4",
     "arch"  : "32-bit"
   },
   "iPad2,1": {
-    "name"  : "iPad 2 (WiFi)",
+    "name"  : "iPad 2 Wi-Fi",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad2,2": {
-    "name"  : "iPad 2 (GSM)",
+    "name"  : "iPad 2 Wi-Fi + 3G (GSM)",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad2,3": {
-    "name"  : "iPad 2 (CDMA)",
+    "name"  : "iPad 2 Wi-Fi + 3G (CDMA)",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad2,4": {
-    "name"  : "iPad 2 (Mid 2012)",
+    "name"  : "iPad 2 Wi-Fi (Mid 2012)",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad2,5": {
-    "name"  : "iPad mini (WiFi)",
+    "name"  : "iPad mini Wi-Fi",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad2,6": {
-    "name"  : "iPad mini (GSM)",
+    "name"  : "iPad mini Wi-Fi + Cellular",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad2,7": {
-    "name"  : "iPad mini (Global)",
+    "name"  : "iPad mini Wi-Fi + Cellular (MM)",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPad3,1": {
-    "name"  : "iPad 3 (WiFi)",
+    "name"  : "iPad (3rd generation) Wi-Fi",
     "soc"   : "A5X",
     "arch"  : "32-bit"
   },
   "iPad3,2": {
-    "name"  : "iPad 3 (CDMA)",
+    "name"  : "iPad (3rd generation) Wi-Fi + Cellular (VZ)",
     "soc"   : "A5X",
     "arch"  : "32-bit"
   },
   "iPad3,3": {
-    "name"  : "iPad 3 (GSM)",
+    "name"  : "iPad (3rd generation) Wi-Fi + Cellular",
     "soc"   : "A5X",
     "arch"  : "32-bit"
   },
   "iPad3,4": {
-    "name"  : "iPad 4 (WiFi)",
+    "name"  : "iPad (4th generation) Wi-Fi",
     "soc"   : "A6X",
     "arch"  : "32-bit"
   },
   "iPad3,5": {
-    "name"  : "iPad 4 (GSM)",
+    "name"  : "iPad (4th generation) Wi-Fi + Cellular",
     "soc"   : "A6X",
     "arch"  : "32-bit"
   },
   "iPad3,6": {
-    "name"  : "iPad 4 (Global)",
+    "name"  : "iPad (4th generation) Wi-Fi + Cellular (MM)",
     "soc"   : "A6X",
     "arch"  : "32-bit"
   },
   "iPad4,1": {
-    "name"  : "iPad Air (WiFi)",
+    "name"  : "iPad Air Wi-Fi",
     "soc"   : "A7",
     "arch"  : "64-bit"
   },
   "iPad4,2": {
-    "name"  : "iPad Air (Cellular)",
+    "name"  : "iPad Air Wi-Fi + Cellular",
     "soc"   : "A7",
     "arch"  : "64-bit"
   },
   "iPad4,3": {
-    "name"  : "iPad Air (China)",
+    "name"  : "iPad Air Wi-Fi + Cellular (TD-LTE)",
     "soc"   : "A7",
     "arch"  : "64-bit"
   },
   "iPad4,4": {
-    "name"  : "iPad mini 2 (WiFi)",
+    "name"  : "iPad mini 2 Wi-Fi",
     "soc"   : "A7",
     "arch"  : "64-bit"
   },
   "iPad4,5": {
-    "name"  : "iPad mini 2 (Cellular)",
+    "name"  : "iPad mini 2 Wi-Fi + Cellular",
     "soc"   : "A7",
     "arch"  : "64-bit"
   },
   "iPad4,6": {
-    "name"  : "iPad mini 2 (China)",
+    "name"  : "iPad mini 2 Wi-Fi + Cellular (TD-LTE)",
     "soc"   : "A7",
     "arch"  : "64-bit"
   },
   "iPad4,7": {
-    "name"  : "iPad mini 3 (WiFi)",
+    "name"  : "iPad mini 3 Wi-Fi",
     "soc"   : "A8",
     "arch"  : "64-bit"
   },
   "iPad4,8": {
-    "name"  : "iPad mini 3 (Cellular)",
+    "name"  : "iPad mini 3 Wi-Fi + Cellular",
     "soc"   : "A8",
     "arch"  : "64-bit"
   },
   "iPad4,9": {
-    "name"  : "iPad mini 3 (China)",
+    "name"  : "iPad mini 3 Wi-Fi + Cellular (TD-LTE)",
     "soc"   : "A8",
     "arch"  : "64-bit"
   },
   "iPad5,1": {
-    "name"  : "iPad mini 4 (WiFi)",
+    "name"  : "iPad mini 4 Wi-Fi",
     "soc"   : "A8",
     "arch"  : "64-bit"
   },
   "iPad5,2": {
-    "name"  : "iPad mini 4 (Cellular)",
+    "name"  : "iPad mini 4 Wi-Fi + Cellular",
     "soc"   : "A8",
     "arch"  : "64-bit"
   },
   "iPad5,3": {
-    "name"  : "iPad Air 2 (Wifi)",
+    "name"  : "iPad Air 2 Wi-Fi",
     "soc"   : "A8X",
     "arch"  : "64-bit"
   },
   "iPad5,4": {
-    "name"  : "iPad Air 2 (Cellular)",
+    "name"  : "iPad Air 2 Wi-Fi + Cellular",
     "soc"   : "A8X",
     "arch"  : "64-bit"
   },
   "iPad6,3": {
-    "name"  : "iPad Pro (9.7-inch, WiFi)",
+    "name"  : "iPad Pro (9.7-inch) Wi-Fi",
     "soc"   : "A9X",
     "arch"  : "64-bit"
   },
   "iPad6,4": {
-    "name"  : "iPad Pro (9.7-inch, Cellular)",
+    "name"  : "iPad Pro (9.7-inch) Wi-Fi + Cellular",
     "soc"   : "A9X",
     "arch"  : "64-bit"
   },
   "iPad6,7": {
-    "name"  : "iPad Pro (12.9-inch, WiFi)",
+    "name"  : "iPad Pro (12.9-inch) Wi-Fi",
     "soc"   : "A9X",
     "arch"  : "64-bit"
   },
   "iPad6,8": {
-    "name"  : "iPad Pro (12.9-inch, Cellular)",
+    "name"  : "iPad Pro (12.9-inch) Wi-Fi + Cellular",
     "soc"   : "A9X",
     "arch"  : "64-bit"
   },
   "iPad6,11": {
-    "name"  : "iPad 5 (WiFi)",
+    "name"  : "iPad (5th generation) Wi-Fi",
     "soc"   : "A9",
     "arch"  : "64-bit"
   },
   "iPad6,12": {
-    "name"  : "iPad 5 (Cellular)",
+    "name"  : "iPad (5th generation) Wi-Fi + Cellular",
     "soc"   : "A9",
     "arch"  : "64-bit"
   },
   "iPad7,1": {
-    "name"  : "iPad Pro 2 (12.9-inch, WiFi)",
+    "name"  : "iPad Pro (12.9-inch) (2nd generation) Wi-Fi",
     "soc"   : "A10X",
     "arch"  : "64-bit"
   },
   "iPad7,2": {
-    "name"  : "iPad Pro 2 (12.9-inch, Cellular)",
+    "name"  : "iPad Pro (12.9-inch) (2nd generation) Wi-Fi + Cellular",
     "soc"   : "A10X",
     "arch"  : "64-bit"
   },
   "iPad7,3": {
-    "name"  : "iPad Pro 2 (10.5-inch, WiFi)",
+    "name"  : "iPad Pro 2 (10.5-inch) Wi-Fi",
     "soc"   : "A10X",
     "arch"  : "64-bit"
   },
   "iPad7,4": {
-    "name"  : "iPad Pro 2 (10.5-inch, Cellular)",
+    "name"  : "iPad Pro 2 (10.5-inch) Wi-Fi + Cellular",
     "soc"   : "A10X",
     "arch"  : "64-bit"
   },
   "iPad7,5": {
-    "name"  : "iPad 6 (WiFi)",
+    "name"  : "iPad (6th generation) Wi-Fi",
     "soc"   : "A10",
     "arch"  : "64-bit"
   },
   "iPad7,6": {
-    "name"  : "iPad 6 (Cellular)",
+    "name"  : "iPad (6th generation) Wi-Fi + Cellular",
     "soc"   : "A10",
     "arch"  : "64-bit"
   },
   "iPad7,11": {
-    "name"  : "iPad 7 (WiFi)",
+    "name"  : "iPad (7th generation) Wi-Fi",
     "soc"   : "A10",
     "arch"  : "64-bit"
   },
   "iPad7,12": {
-    "name"  : "iPad 7 (Cellular)",
+    "name"  : "iPad (7th generation) Wi-Fi + Cellular",
     "soc"   : "A10",
     "arch"  : "64-bit"
   },
   "iPad8,1": {
-    "name"  : "iPad Pro 3 (11-inch, Wifi)",
+    "name"  : "iPad Pro 11-inch Wi-Fi",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,2": {
-    "name"  : "iPad Pro 3 (11-inch, Wifi, 1TB Model)",
+    "name"  : "iPad Pro 11-inch Wi-Fi (1TB)",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,3": {
-    "name"  : "iPad Pro 3 (11-inch, Cellular)",
+    "name"  : "iPad Pro 11-inch Wi-Fi + Cellular",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,4": {
-    "name"  : "iPad Pro 3 (11-inch, Cellular, 1TB Model)",
+    "name"  : "iPad Pro 11-inch Wi-Fi + Cellular (1TB)",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,5": {
-    "name"  : "iPad Pro 3 (12.9-inch, WiFi)",
+    "name"  : "iPad Pro 12.9-inch (3rd generation) Wi-Fi",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,6": {
-    "name"  : "iPad Pro 3 (12.9-inch, WiFi, 1TB Model)",
+    "name"  : "iPad Pro 12.9-inch (3rd generation) Wi-Fi (1TB)",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,7": {
-    "name"  : "iPad Pro 3 (12.9-inch, Cellular)",
+    "name"  : "iPad Pro 12.9-inch (3rd generation) Wi-Fi + Cellular",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,8": {
-    "name"  : "iPad Pro 3 (12.9-inch, Cellular, 1TB Model)",
+    "name"  : "iPad Pro 12.9-inch (3rd generation) Wi-Fi + Cellular (1TB)",
     "soc"   : "A12X",
     "arch"  : "64-bit"
   },
   "iPad8,9": {
-    "name"  : "iPad Pro 4 (11-inch, WiFi)",
+    "name"  : "iPad Pro 11-inch (2nd generation) Wi-Fi",
     "soc"   : "A12Z",
     "arch"  : "64-bit"
   },
   "iPad8,10": {
-    "name"  : "iPad Pro 4 (11-inch, Cellular)",
+    "name"  : "iPad Pro 11-inch (2nd generation) Wi-Fi + Cellular",
     "soc"   : "A12Z",
     "arch"  : "64-bit"
   },
   "iPad8,11": {
-    "name"  : "iPad Pro 4 (12.9-inch, WiFi)",
+    "name"  : "iPad Pro 12.9-inch (4th generation) Wi-Fi",
     "soc"   : "A12Z",
     "arch"  : "64-bit"
   },
   "iPad8,12": {
-    "name"  : "iPad Pro 4 (12.9-inch, Cellular)",
+    "name"  : "iPad Pro 12.9-inch (4th generation) Wi-Fi + Cellular",
     "soc"   : "A12Z",
     "arch"  : "64-bit"
   },
   "iPad11,1": {
-    "name"  : "iPad mini 5 (WiFi)",
+    "name"  : "iPad mini (5th generation) Wi-Fi",
     "soc"   : "A12",
     "arch"  : "64-bit"
   },
   "iPad11,2": {
-    "name"  : "iPad mini 5 (Cellular)",
+    "name"  : "iPad mini (5th generation) Wi-Fi + Cellular",
     "soc"   : "A12",
     "arch"  : "64-bit"
   },
   "iPad11,3": {
-    "name"  : "iPad Air 3 (WiFi)",
+    "name"  : "iPad Air (3rd generation) Wi-Fi",
     "soc"   : "A12",
     "arch"  : "64-bit"
   },
   "iPad11,4": {
-    "name"  : "iPad Air 3 (Cellular)",
+    "name"  : "iPad Air (3rd generation) Wi-Fi + Cellular",
     "soc"   : "A12",
     "arch"  : "64-bit"
   },
   "iPad11,6": {
-    "name"  : "iPad 8 (WiFi)",
+    "name"  : "iPad (8th generation) Wi-Fi",
     "soc"   : "A12",
     "arch"  : "64-bit"
   },
   "iPad11,7": {
-    "name"  : "iPad 8 (Cellular)",
+    "name"  : "iPad (8th generation) Wi-Fi + Cellular",
     "soc"   : "A12",
     "arch"  : "64-bit"
   },
   "iPad12,1": {
-    "name"  : "iPad 9 (WiFi)",
+    "name"  : "iPad (9th generation) Wi-Fi",
     "soc"   : "A13",
     "arch"  : "64-bit"
   },
   "iPad12,2": {
-    "name"  : "iPad 9 (Cellular)",
+    "name"  : "iPad (9th generation) Wi-Fi + Cellular",
     "soc"   : "A13",
     "arch"  : "64-bit"
   },
   "iPad13,1": {
-    "name"  : "iPad Air 4 (WiFi)",
+    "name"  : "iPad Air (4th generation) Wi-Fi",
     "soc"   : "A14",
     "arch"  : "64-bit"
   },
   "iPad13,2": {
-    "name"  : "iPad Air 4 (Cellular)",
+    "name"  : "iPad Air (4th generation) Wi-Fi + Cellular",
     "soc"   : "A14",
     "arch"  : "64-bit"
   },
   "iPad13,4": {
-    "name"  : "iPad Pro 5 (11-inch, WiFi)",
+    "name"  : "iPad Pro 11-inch (3rd generation) Wi-Fi",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,5": {
-    "name"  : "iPad Pro 5 (11-inch, US)",
+    "name"  : "iPad Pro 11-inch (3rd generation) Wi-Fi + Cellular with mmWave",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,6": {
-    "name"  : "iPad Pro 5 (11-inch, Cellular)",
+    "name"  : "iPad Pro 11-inch (3rd generation) Wi-Fi + Cellular",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,7": {
-    "name"  : "iPad Pro 5 (11-inch, China)",
+    "name"  : "iPad Pro 11-inch (3rd generation) Wi-Fi + Cellular (China mainland)",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,8": {
-    "name"  : "iPad Pro 5 (12.9-inch, WiFi)",
+    "name"  : "iPad Pro 12.9-inch (5th generation) Wi-Fi",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,9": {
-    "name"  : "iPad Pro 5 (12.9-inch, US)",
+    "name"  : "iPad Pro 12.9-inch (5th generation) Wi-Fi + Cellular with mmWave",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,10": {
-    "name"  : "iPad Pro 5 (12.9-inch, Cellular)",
+    "name"  : "iPad Pro 12.9-inch (5th generation) Wi-Fi + Cellular",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad13,11": {
-    "name"  : "iPad Pro 5 (12.9-inch, China)",
+    "name"  : "iPad Pro 12.9-inch (5th generation) Wi-Fi + Cellular (China mainland)",
     "soc"   : "M1",
     "arch"  : "64-bit"
   },
   "iPad14,1": {
-    "name"  : "iPad mini 6 (WiFi)",
+    "name"  : "iPad mini (6th generation) Wi-Fi",
     "soc"   : "A15",
     "arch"  : "64-bit"
   },
   "iPad14,2": {
-    "name"  : "iPad mini 6 (Cellular)",
+    "name"  : "iPad mini (6th generation) Wi-Fi + Cellular",
     "soc"   : "A15",
     "arch"  : "64-bit"
   },
   "iPod1,1": {
-    "name"  : "iPod touch 1G",
+    "name"  : "iPod touch",
     "soc"   : "APL0098",
     "arch"  : "32-bit"
   },
   "iPod2,1": {
-    "name"  : "iPod touch 2G",
+    "name"  : "iPod touch (2nd generation)",
     "soc"   : "APL0278",
     "arch"  : "32-bit"
   },
   "iPod3,1": {
-    "name"  : "iPod touch 3",
+    "name"  : "iPod touch (3rd generation)",
     "soc"   : "APL2298",
     "arch"  : "32-bit"
   },
   "iPod4,1": {
-    "name"  : "iPod touch 4",
+    "name"  : "iPod touch (4th generation)",
     "soc"   : "A4",
     "arch"  : "32-bit"
   },
   "iPod5,1": {
-    "name"  : "iPod touch 5",
+    "name"  : "iPod touch (5th generation)",
     "soc"   : "A5",
     "arch"  : "32-bit"
   },
   "iPod7,1": {
-    "name"  : "iPod touch 6",
+    "name"  : "iPod touch (6th generation)",
     "soc"   : "A8",
     "arch"  : "64-bit"
   },
   "iPod9,1": {
-    "name"  : "iPod touch 7",
+    "name"  : "iPod touch (7th generation)",
     "soc"   : "A8",
     "arch"  : "64-bit"
   }
