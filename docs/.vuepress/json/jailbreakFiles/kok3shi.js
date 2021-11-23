3ab537fe40436999fd1a434ff5640fc8c7e02ab2
emiyl <me@emiyl.com>
2021-11-23 23:06:25 +0000

    added kok3shi

---
+++ docs/.vuepress/json/jailbreakFiles/kok3shi.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "name": "kok3shi",
+  "compatibility": [
+    {
+      "firmwares": [
+        "13E233", // 9.3
+        "13E237", // 9.3
+        "13E238", // 9.3.1
+        "13F69", // 9.3.2
+        "13G34", // 9.3.3
+        "13G35", // 9.3.4
+        "13G36", // 9.3.5
+      ],
+      "devices": [
+      "iPhone6,1", // iPhone 5s (GSM), A7
+      "iPhone6,2", // iPhone 5s (CDMA), A7
+      "iPhone7,1", // iPhone 6 Plus, A8
+      "iPhone7,2", // iPhone 6, A8
+      "iPad4,1", // iPad Air Wi-Fi, A7
+      "iPad4,2", // iPad Air Wi-Fi + Cellular, A7
+      "iPad4,3", // iPad Air Wi-Fi + Cellular (TD-LTE), A7
+      "iPad4,4", // iPad mini 2 Wi-Fi, A7
+      "iPad4,5", // iPad mini 2 Wi-Fi + Cellular, A7
+      "iPad4,6", // iPad mini 2 Wi-Fi + Cellular (TD-LTE), A7
+      "iPad4,7", // iPad mini 3 Wi-Fi, A8
+      "iPad4,8", // iPad mini 3 Wi-Fi + Cellular, A8
+      "iPad4,9", // iPad mini 3 Wi-Fi + Cellular (TD-LTE), A8
+      "iPad5,1", // iPad mini 4 Wi-Fi, A8
+      "iPad5,2", // iPad mini 4 Wi-Fi + Cellular, A8
+      "iPad5,3", // iPad Air 2 Wi-Fi, A8X
+      "iPad5,4", // iPad Air 2 Wi-Fi + Cellular, A8X
+      "iPod7,1", // iPod touch (6th generation), A8
+      ]
+    },
+  ]
+}
