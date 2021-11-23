b2debe737ce8c7ee943d1af6d4b1de838ad14336
emiyl <me@emiyl.com>
2021-11-23 22:34:31 +0000

    added daibutsu

---
+++ docs/.vuepress/json/jailbreakFiles/daibutsu.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "name": "daibutsu",
+  "compatibility": [
+    {
+      "firmwares": [
+        "13A344", // 9.0
+        "13A404", // 9.0.1
+        "13A452", // 9.0.2
+        "13B143", // 9.1
+      ],
+      "devices": [
+        "iPhone4,1", // iPhone 4S, A5
+        "iPhone5,1", // iPhone 5 (GSM), A6
+        "iPhone5,2", // iPhone 5 (CDMA), A6
+        "iPhone5,3", // iPhone 5c (GSM), A6
+        "iPhone5,4", // iPhone 5c (CDMA), 
+        "iPad2,1", // iPad 2 Wi-Fi, A5
+        "iPad2,2", // iPad 2 Wi-Fi + 3G (GSM), A5
+        "iPad2,3", // iPad 2 Wi-Fi + 3G (CDMA), A5
+        "iPad2,4", // iPad 2 Wi-Fi (Mid 2012), A5
+        "iPad2,5", // iPad mini Wi-Fi, A5
+        "iPad2,5", // iPad mini Wi-Fi + Cellular, A5
+        "iPad2,5", // iPad mini Wi-Fi + Cellular (MM), A5
+        "iPad3,1", // iPad (3rd generation) Wi-Fi, A5X
+        "iPad3,2", // iPad (3rd generation) Wi-Fi + Cellular (VZ), A5X
+        "iPad3,3", // iPad (3rd generation) Wi-Fi + Cellular, A5X
+        "iPad3,4", // iPad (4th generation) Wi-Fi, A6X
+        "iPad3,5", // iPad (4th generation) Wi-Fi + Cellular, A6X
+        "iPad3,6", // iPad (4th generation) Wi-Fi + Cellular (MM), A6X
+        "iPod5,1", // iPod touch (5th generation), A5
+        "iPod7,1", // iPod touch (7th generation), A8
+      ]
+    },
+  ]
+}
