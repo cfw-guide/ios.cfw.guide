ee4091fa6fabad5627751210fa3668f424aa3b4c
emiyl <me@emiyl.com>
2021-11-23 22:38:23 +0000

    added n1ghtshade

---
+++ docs/.vuepress/json/jailbreakFiles/n1ghtshade.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "name": "n1ghtshade",
+  "compatibility": [
+    {
+      "firmwares": [
+        "13A344", // 9.0
+        "13A404", // 9.0.1
+        "13A452", // 9.0.2
+        "13B143", // 9.1
+        "13C75", // 9.2
+        "13D15", // 9.2.1
+        "13E233", // 9.3
+        "13E237", // 9.3
+        "13E238", // 9.3.1
+        "13F69", // 9.3.2
+        "13G34", // 9.3.3
+        "13G35", // 9.3.4
+        "13G36", // 9.3.5
+      ],
+      "devices": [
+        "iPhone5,1", // iPhone 5 (GSM), A6
+        "iPhone5,2", // iPhone 5 (CDMA), A6
+        "iPhone5,3", // iPhone 5c (GSM), A6
+        "iPhone5,4", // iPhone 5c (CDMA), A6
+        "iPad3,4", // iPad (4th generation) Wi-Fi, A6X
+        "iPad3,5", // iPad (4th generation) Wi-Fi + Cellular, A6X
+        "iPad3,6", // iPad (4th generation) Wi-Fi + Cellular (MM), A6X
+      ]
+    },
+  ]
+}
