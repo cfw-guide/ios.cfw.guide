8ff2768690f711aa59665e7aa31cd1ac37fa964f
emiyl <me@emiyl.com>
2021-11-23 22:22:02 +0000

    added tetherbootx32 and p0insettia

---
+++ docs/.vuepress/json/jailbreakFiles/p0insettia.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "name": "p0insettia",
+  "compatibility": [
+    {
+      "firmwares": [
+        "14G61", // 10.3.4
+      ],
+      "devices": [
+        "iPhone5,1", // iPhone 5 (GSM), A6
+        "iPhone5,2", // iPhone 5 (CDMA), A6
+      ]
+    }
+  ]
+}
