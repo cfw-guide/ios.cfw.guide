636ce9ed9868914d63f303965e371b4847f06f24
emiyl <me@emiyl.com>
2021-11-23 23:10:23 +0000

    added cherryflowerJB

---
+++ docs/.vuepress/json/jailbreakFiles/cherryflowerJB.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "name": "cherryflowerJB",
+  "compatibility": [
+    {
+      "firmwares": [
+        "13G36", // 9.3.5
+      ],
+      "devices": [
+        "iPhone5,1", // iPhone 5 (GSM), A6
+        "iPhone5,2", // iPhone 5 (CDMA), A6
+        "iPhone5,3", // iPhone 5c (GSM), A6
+        "iPhone5,4", // iPhone 5c (CDMA), A6
+      ]
+    },
+  ]
+}
