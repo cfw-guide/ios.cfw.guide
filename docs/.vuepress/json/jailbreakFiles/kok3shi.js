5f61f01edfa5cf83540ede87c0735884d692b0bd
emiyl <me@emiyl.com>
2021-11-23 23:09:39 +0000

    added phoenix

--- docs/.vuepress/json/jailbreakFiles/kok3shi.js
+++ docs/.vuepress/json/jailbreakFiles/kok3shi.js
@@ -12,24 +12,24 @@ module.exports = {
         "13G36", // 9.3.5
       ],
       "devices": [
-      "iPhone6,1", // iPhone 5s (GSM), A7
-      "iPhone6,2", // iPhone 5s (CDMA), A7
-      "iPhone7,1", // iPhone 6 Plus, A8
-      "iPhone7,2", // iPhone 6, A8
-      "iPad4,1", // iPad Air Wi-Fi, A7
-      "iPad4,2", // iPad Air Wi-Fi + Cellular, A7
-      "iPad4,3", // iPad Air Wi-Fi + Cellular (TD-LTE), A7
-      "iPad4,4", // iPad mini 2 Wi-Fi, A7
-      "iPad4,5", // iPad mini 2 Wi-Fi + Cellular, A7
-      "iPad4,6", // iPad mini 2 Wi-Fi + Cellular (TD-LTE), A7
-      "iPad4,7", // iPad mini 3 Wi-Fi, A8
-      "iPad4,8", // iPad mini 3 Wi-Fi + Cellular, A8
-      "iPad4,9", // iPad mini 3 Wi-Fi + Cellular (TD-LTE), A8
-      "iPad5,1", // iPad mini 4 Wi-Fi, A8
-      "iPad5,2", // iPad mini 4 Wi-Fi + Cellular, A8
-      "iPad5,3", // iPad Air 2 Wi-Fi, A8X
-      "iPad5,4", // iPad Air 2 Wi-Fi + Cellular, A8X
-      "iPod7,1", // iPod touch (6th generation), A8
+        "iPhone6,1", // iPhone 5s (GSM), A7
+        "iPhone6,2", // iPhone 5s (CDMA), A7
+        "iPhone7,1", // iPhone 6 Plus, A8
+        "iPhone7,2", // iPhone 6, A8
+        "iPad4,1", // iPad Air Wi-Fi, A7
+        "iPad4,2", // iPad Air Wi-Fi + Cellular, A7
+        "iPad4,3", // iPad Air Wi-Fi + Cellular (TD-LTE), A7
+        "iPad4,4", // iPad mini 2 Wi-Fi, A7
+        "iPad4,5", // iPad mini 2 Wi-Fi + Cellular, A7
+        "iPad4,6", // iPad mini 2 Wi-Fi + Cellular (TD-LTE), A7
+        "iPad4,7", // iPad mini 3 Wi-Fi, A8
+        "iPad4,8", // iPad mini 3 Wi-Fi + Cellular, A8
+        "iPad4,9", // iPad mini 3 Wi-Fi + Cellular (TD-LTE), A8
+        "iPad5,1", // iPad mini 4 Wi-Fi, A8
+        "iPad5,2", // iPad mini 4 Wi-Fi + Cellular, A8
+        "iPad5,3", // iPad Air 2 Wi-Fi, A8X
+        "iPad5,4", // iPad Air 2 Wi-Fi + Cellular, A8X
+        "iPod7,1", // iPod touch (6th generation), A8
       ]
     },
   ]
