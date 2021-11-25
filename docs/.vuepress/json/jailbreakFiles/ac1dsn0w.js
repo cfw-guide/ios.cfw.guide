84fe4beddd37946772a00558a2567ce3d64caeed
emiyl <me@emiyl.com>
2021-11-25 19:56:18 +0000

    added ac1dsnow

---
+++ docs/.vuepress/json/jailbreakFiles/ac1dsn0w.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "name": "Ac1dsn0w",
+  "compatibility": [
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPad1,1", // iPad
+        "iPod4,1", // iPod touch (4th generation)
+      ]
+    },
+    {
+      "firmwares": [
+        "9A405", // 5.0.1
+      ],
+      "devices": [
+        "iPod3,1", // iPod touch (3rd generation)
+      ]
+    }
+  ]
+}
