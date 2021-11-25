652dadaaf3265aae058ac09ff25885ecd19518fd
emiyl <me@emiyl.com>
2021-11-25 20:18:43 +0000

    added pwnagetool

---
+++ docs/.vuepress/json/jailbreakFiles/pwnagetool.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "name": "PwnageTool",
+  "compatibility": [
+    {
+      "firmwares": [
+        "9A405", // 5.0.1
+        "9B206", // 5.1.1
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    },
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B176", // 5.1
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 only
+      ],
+      "devices": [
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+      ]
+    },
+    {
+      "firmwares": [
+        "9A405", // 5.0.1
+      ],
+      "devices": [
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
+      ]
+    },
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B206", // 5.1.1
+      ],
+      "devices": [
+        "iPad1,1", // iPad
+      ]
+    }
+  ]
+}
