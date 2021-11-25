20946c2e2a78cf7c2cd6ca4e65689a5b40f6fe2c
emiyl <me@emiyl.com>
2021-11-25 20:43:16 +0000

    added ios 4 to redsn0w

--- docs/.vuepress/json/jailbreakFiles/redsn0w.js
+++ docs/.vuepress/json/jailbreakFiles/redsn0w.js
@@ -3,6 +3,25 @@ module.exports = {
   "compatibility": [
     {
       "firmwares": [
+        "8A293", // 4.0
+        "8A306", // 4.0.1
+        "8A400", // 4.0.2
+        "8B117", // 4.1
+        "8C148", // 4.2.1
+      ],
+      "devices": [
+        "iPhone1,2", // iPhone 3G
+        "iPod2,1", // iPod touch (2nd generation)
+      ]
+    },
+    {
+      "firmwares": [
+        "8F190", // 4.3
+        "8G4", // 4.3.1
+        "8H7", // 4.3.2
+        "8J2", // 4.3.3
+        "8K2", // 4.3.4
+        "8L1", // 4.3.5
         "9A334", // 5.0
         "9A405", // 5.0.1
         "9B176", // 5.1
@@ -20,6 +39,40 @@ module.exports = {
       ],
       "devices": [
         "iPhone2,1", // iPhone 3GS
+      ]
+    },
+    {
+      "firmwares": [
+        "8B117", // 4.1
+        "8B118", // 4.1, iPod touch 4 only
+        "8C148", // 4.2.1
+        "8E200", // 4.2.6, iPhone 4 (CDMA) only
+        "8E303", // 4.2.7, iPhone 4 (CDMA) only
+        "8E401", // 4.2.8, iPhone 4 (CDMA) only
+        "8E501", // 4.2.9, iPhone 4 (CDMA) only
+        "8E600", // 4.2.10, iPhone 4 (CDMA) only
+        "8F190", // 4.3
+        "8G4", // 4.3.1
+        "8H7", // 4.3.2
+        "8J2", // 4.3.3
+        "8K2", // 4.3.4
+        "8L1", // 4.3.5
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B176", // 5.1
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 only
+        "10A403", // 6.0
+        "10A523", // 6.0.1
+        "10B141", // 6.1
+        "10B144", // 6.1, iPhone 4 (GSM) and iPod touch 4 only
+        "10B146", // 6.1.2
+        "10B329", // 6.1.3
+        "10B350", // 6.1.4
+        "10B400", // 6.1.5
+        "10B500", // 6.1.6
+      ],
+      "devices": [
         "iPhone3,1", // iPhone 4 (GSM), A4
         "iPhone3,2", // iPhone 4 (GSM, 2012), A4
         "iPhone3,3", // iPhone 4 (CDMA), A4
