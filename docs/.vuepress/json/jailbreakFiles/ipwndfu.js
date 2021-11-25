b1165c83c39ac9fce5078a2b892bdda1a0ccc0a6
emiyl <me@emiyl.com>
2021-11-25 11:34:42 +0000

    added ipwndfu

---
+++ docs/.vuepress/json/jailbreakFiles/ipwndfu.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "name": "ipwndfu",
+  "compatibility": [
+    {
+      "firmwares": [
+        "10A403", // 6.0
+        "10A523", // 6.0.1
+        "10B141", // 6.1
+        "10B146", // 6.1.2
+        "10B329", // 6.1.3
+        "10B500", // 6.1.6
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    },
+  ]
+}
