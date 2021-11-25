0600eda4c38a6673514cec9c68cbf36461c165ee
emiyl <me@emiyl.com>
2021-11-25 11:41:24 +0000

    added sn0wbreeze

---
+++ docs/.vuepress/json/jailbreakFiles/sn0wbreeze.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "name": "sn0wbreeze",
+  "compatibility": [
+    {
+      "firmwares": [
+        "10A403", // 6.0
+        "10A523", // 6.0.1
+        "10B141", // 6.1
+        "10B144", // 6.1, iPhone 4 (GSM) and iPod touch 4 only
+        "10B146", // 6.1.2
+        "10B329", // 6.1.3
+      ],
+      "devices": [
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,2", // iPhone 4 (GSM, 2012), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+      ]
+    },
+  ]
+}
