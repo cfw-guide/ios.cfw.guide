0ee6fed9abb56cbabb4aae00bd84cd13c6303c6e
emiyl <me@emiyl.com>
2021-11-25 10:08:42 +0000

    phoenix redirects

--- docs/.vuepress/plugins/dynamicPages/lib/jailbreakChart/index.js
+++ docs/.vuepress/plugins/dynamicPages/lib/jailbreakChart/index.js
@@ -1,13 +1,19 @@
 const jbList = require('../../../../json/jailbreak');
+const jbPath = '/chart/jailbreak/';
 var page = [];
 
 for (var jb in jbList) {
+  var redirects = ['/' + jbList[jb].name];
+  if (jbList[jb].name == 'Phœnix') {
+    redirects.push('/phoenix');
+    redirects.push(jbPath + 'phoenix')
+  }
   page.push({
-    path: '/chart/jailbreak/' + jbList[jb].name + '.html',
+    path: jbPathS + jbList[jb].name + '.html',
     frontmatter: {
       title: jbList[jb].name,
       description: 'Compatible firmwares for ' + jbList[jb].name,
-      redirect_from: '/' + jbList[jb].name,
+      redirect_from: redirects,
       sidebar: false
     },
     content: require('./content')(jbList[jb].name)
