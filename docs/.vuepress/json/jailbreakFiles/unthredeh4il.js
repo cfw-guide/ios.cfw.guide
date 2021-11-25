19ae98e69ecae2ed6a89b7423e8c4aa78ce50000
emiyl <me@emiyl.com>
2021-11-25 19:58:39 +0000

    added unthredeh4il

---
+++ docs/.vuepress/json/jailbreakFiles/unthredeh4il.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "name": "unthredeh4il",
+  "compatibility": [
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    },
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B176", // 5.1
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 only
+      ],
+      "devices": [
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPad1,1", // iPad
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
+      ]
+    }
+  ]
+}
