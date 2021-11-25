0594bd37a25aa53af5f30bcd0fd5dea1792043fc
emiyl <me@emiyl.com>
2021-11-25 20:05:33 +0000

    added ios 5 to sn0wbreeze

--- docs/.vuepress/json/jailbreakFiles/sn0wbreeze.js
+++ docs/.vuepress/json/jailbreakFiles/sn0wbreeze.js
@@ -3,6 +3,11 @@ module.exports = {
   "compatibility": [
     {
       "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B176", // 5.1
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 only
         "10A403", // 6.0
         "10A523", // 6.0.1
         "10B141", // 6.1
@@ -14,6 +19,27 @@ module.exports = {
         "iPhone3,1", // iPhone 4 (GSM), A4
         "iPhone3,2", // iPhone 4 (GSM, 2012), A4
         "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
+      ]
+    },
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    },
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B206", // 5.1.1
+      ],
+      "devices": [
+        "iPad1,1", // iPad
       ]
     },
   ]
