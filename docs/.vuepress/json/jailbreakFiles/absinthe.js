ef12424be28fbbb12503163eeae9355f948fbfdd
emiyl <me@emiyl.com>
2021-11-25 20:13:58 +0000

    added absinthe

---
+++ docs/.vuepress/json/jailbreakFiles/absinthe.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "name": "Absinthe",
+  "compatibility": [
+    {
+      "firmwares": [
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 (GSM) only
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPad1,1", // iPad
+        "iPad2,4", // iPad 2 Wi-Fi (Mid 2012), A5
+        "iPad3,1", // iPad (3rd generation) Wi-Fi, A5X
+        "iPad3,2", // iPad (3rd generation) Wi-Fi + Cellular (VZ), A5X
+        "iPad3,3", // iPad (3rd generation) Wi-Fi + Cellular, A5X
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
+      ]
+    },
+    {
+      "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9A406", // 5.0.1, iPhone 4S only
+      ],
+      "devices": [
+        "iPhone4,1", // iPhone 4S, A5
+      ]
+    },
+    {
+      "firmwares": [
+        "9A405", // 5.0.1
+          "9B206", // 5.1.1
+      ],
+      "devices": [
+        "iPad2,1", // iPad 2 Wi-Fi, A5
+        "iPad2,2", // iPad 2 Wi-Fi + 3G (GSM), A5
+      ]
+    }
+  ]
+}
