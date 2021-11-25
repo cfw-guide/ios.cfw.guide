d65fd9ddb566f07bd45d0e1e53c418fe6f34f18a
emiyl <me@emiyl.com>
2021-11-25 11:57:22 +0000

    added ios 5 to redsn0w

--- docs/.vuepress/json/jailbreakFiles/redsn0w.js
+++ docs/.vuepress/json/jailbreakFiles/redsn0w.js
@@ -3,6 +3,11 @@ module.exports = {
   "compatibility": [
     {
       "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B176", // 5.1
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 only
         "10A403", // 6.0
         "10A523", // 6.0.1
         "10B141", // 6.1
@@ -18,7 +23,20 @@ module.exports = {
         "iPhone3,1", // iPhone 4 (GSM), A4
         "iPhone3,2", // iPhone 4 (GSM, 2012), A4
         "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPad1,1", // iPad
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
       ]
     },
+    {
+      "firmwares": [
+        "9A405", // 5.0.1
+        "9A406", // 5.0.1, iPhone 4S only
+        "9B206", // 5.1.1
+      ],
+      "devices": [
+        "iPhone4,1", // iPhone 4S, A5
+      ]
+    }
   ]
 }
