4d2c0ec85128ad5a9496328c94b40faba5ff2720
emiyl <me@emiyl.com>
2021-11-25 19:54:25 +0000

    added ios 5 to ipwndfu

--- docs/.vuepress/json/jailbreakFiles/ipwndfu.js
+++ docs/.vuepress/json/jailbreakFiles/ipwndfu.js
@@ -3,6 +3,9 @@ module.exports = {
   "compatibility": [
     {
       "firmwares": [
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9B176", // 5.1
         "10A403", // 6.0
         "10A523", // 6.0.1
         "10B141", // 6.1
@@ -14,5 +17,15 @@ module.exports = {
         "iPhone2,1", // iPhone 3GS
       ]
     },
+    {
+      "firmwares": [
+        "9B206", // 5.1.1
+        "9B208", // 5.1.1, iPhone 4 (GSM) only
+      ],
+      "devices": [
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+      ]
+    }
   ]
 }
