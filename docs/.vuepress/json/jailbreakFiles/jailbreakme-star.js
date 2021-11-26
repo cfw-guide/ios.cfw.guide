f5b2ed7462759a3bfaef3aff5f7fee2881a16f8f
emiyl <me@emiyl.com>
2021-11-26 11:12:00 +0000

    added jailbreakme star

---
+++ docs/.vuepress/json/jailbreakFiles/jailbreakme-star.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "name": "JailbreakMe Star",
+  "compatibility": [
+    {
+      "firmwares": [
+        "8A293", // 4.0
+        "8A306", // 4.0.1
+      ],
+      "devices": [
+        "iPhone1,2", // iPhone 3G
+        "iPhone2,1", // iPhone 3GS
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPod2,1", // iPod touch (2nd generation)
+        "iPod3,1", // iPod touch (3rd generation)
+      ]
+    },
+  ]
+}
