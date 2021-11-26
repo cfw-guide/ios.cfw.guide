8a1c333c767ebc7fe725eaa73b451538d35b2e88
emiyl <me@emiyl.com>
2021-11-26 11:21:40 +0000

    added limera1n

---
+++ docs/.vuepress/json/jailbreakFiles/limera1n.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "name": "limera1n",
+  "compatibility": [
+    {
+      "firmwares": [
+        "8A293", // 4.0
+        "8A306", // 4.0.1
+        "8A400", // 4.0.2
+        "8B117", // 4.1
+        "8B118", // 4.1, iPod touch 4 only
+        "8C148a", // 4.2.1, iPhone 3GS only
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPad1,1", // iPad
+        "iPod2,1", // iPod touch (2nd generation)
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
+      ]
+    },
+    {
+      "firmwares": [
+        "8B117", // 4.1
+      ],
+      "devices": [
+        "iPod2,1", // iPod touch (2nd generation)
+      ]
+    },
+  ]
+}
