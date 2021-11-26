7e3518d4ee8ef21df50898bca3445720fa95b585
emiyl <me@emiyl.com>
2021-11-26 12:07:32 +0000

    added spirit and spiritnet

---
+++ docs/.vuepress/json/jailbreakFiles/spirit.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "name": "Spirit",
+  "compatibility": [
+    {
+      "firmwares": [
+        "7D11", // 3.1.2
+        "7E18", // 3.1.3
+        "7B367", // 3.2, iPad only
+      ],
+      "devices": [
+        "iPhone1,1", // iPhone
+        "iPhone1,2", // iPhone 3G
+        "iPhone2,1", // iPhone 3GS
+        "iPad1,1", // iPad
+        "iPod1,1", // iPod touch
+        "iPod2,1", // iPod touch (2nd generation)
+        "iPod3,1", // iPod touch (3rd generation)
+      ]
+    },
+  ]
+}
