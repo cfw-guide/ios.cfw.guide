c61a706195ee32a9f431b148cedff91f87707535
emiyl <me@emiyl.com>
2021-11-26 11:26:26 +0000

    added greenp0ison

---
+++ docs/.vuepress/json/jailbreakFiles/greenp0ison.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "name": "greenp0ison",
+  "compatibility": [
+    {
+      "firmwares": [
+        "8B117", // 4.1
+        "8B118", // 4.1, iPod touch 4 only
+        "8C148", // 4.2.1
+        "8E200", // 4.2.6, iPhone 4 (CDMA) only
+      ],
+      "devices": [
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,3", // iPhone 4 (CDMA), A4
+        "iPod2,1", // iPod touch (2nd generation)
+        "iPod3,1", // iPod touch (3rd generation)
+        "iPod4,1", // iPod touch (4th generation)
+        "iPad1,1", // iPad
+      ]
+    },
+    {
+      "firmwares": [
+        "8C148a", // 4.2.1, iPhone 3GS only
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    }
+  ]
+}
