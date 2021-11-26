bb2e3ee8b74b1c77abb82f1a83bb9bea84299fe6
emiyl <me@emiyl.com>
2021-11-26 14:51:31 +0000

    added ios 2 jailbreaks

---
+++ docs/.vuepress/json/jailbreakFiles/cyw00d.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "name": "Cyw00d",
+  "compatibility": [
+    {
+      "firmwares": [
+        "5H11a", // 2.2.1, iPod touch (2nd gen) only
+      ],
+      "devices": [
+        "iPod2,1", // iPod touch (2nd generation)
+      ]
+    },
+  ]
+}
---
+++ docs/.vuepress/json/jailbreakFiles/redsn0w-lite.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "name": "redsn0w Lite",
+  "compatibility": [
+    {
+      "firmwares": [
+        "5F138", // 2.1.1, iPod touch (2nd gen) only
+      ],
+      "devices": [
+        "iPod2,1", // iPod touch (2nd generation)
+      ]
+    },
+  ]
+}
