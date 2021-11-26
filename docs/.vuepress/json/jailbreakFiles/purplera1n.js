68f45b7e5e6632e344fd5d7c1770384f5e4a24d6
emiyl <me@emiyl.com>
2021-11-26 12:00:26 +0000

    added purplera1n

---
+++ docs/.vuepress/json/jailbreakFiles/purplera1n.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "name": "purplera1n",
+  "compatibility": [
+    {
+      "firmwares": [
+        "7A341", // 3.0
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    }
+  ]
+}
