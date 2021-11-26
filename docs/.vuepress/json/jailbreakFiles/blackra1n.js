f8eb39b9932c97f9e90539866363af5c06320cc6
emiyl <me@emiyl.com>
2021-11-26 12:02:49 +0000

    added blackra1n

---
+++ docs/.vuepress/json/jailbreakFiles/blackra1n.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "name": "blackra1n",
+  "compatibility": [
+    {
+      "firmwares": [
+        "7C144", // 3.1, iPhone only
+        "7C145", // 3.1.1, iPod only
+        "7C146", // 3.1.1, iPod touch 3 only
+        "7D11", // 3.1.2
+      ],
+      "devices": [
+        "iPhone1,1", // iPhone
+        "iPhone1,2", // iPhone 3G
+        "iPod1,1", // iPod touch
+        "iPod2,1", // iPod touch (2nd generation)
+        "iPod3,1", // iPod touch (3rd generation)
+      ]
+    },
+    {
+      "firmwares": [
+      "7E18", // 3.1.3
+      ],
+      "devices": [
+        "iPhone2,1", // iPhone 3GS
+      ]
+    }
+  ]
+}
