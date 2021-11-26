f30b2a36df97b7fd1fa629e417ad96fc1dd2dd91
emiyl <me@emiyl.com>
2021-11-26 12:05:34 +0000

    added ios 3 to jailbreakme star

--- docs/.vuepress/json/jailbreakFiles/jailbreakme-star.js
+++ docs/.vuepress/json/jailbreakFiles/jailbreakme-star.js
@@ -3,13 +3,21 @@ module.exports = {
   "compatibility": [
     {
       "firmwares": [
+        "7D11", // 3.1.2
+        "7E18", // 3.1.3
         "8A293", // 4.0
         "8A306", // 4.0.1
+        "7B367", // 3.2, iPad only
+        "7B405", // 3.2.1, iPad only
+        "7B500", // 3.2.2, iPad only
       ],
       "devices": [
+        "iPhone1,1", // iPhone
         "iPhone1,2", // iPhone 3G
         "iPhone2,1", // iPhone 3GS
         "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPad1,1", // iPad
+        "iPod1,1", // iPod touch
         "iPod2,1", // iPod touch (2nd generation)
         "iPod3,1", // iPod touch (3rd generation)
       ]
