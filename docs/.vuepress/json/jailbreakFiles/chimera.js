fdb128b881eb336c1718b33312e50c0dcda8a705
emiyl <me@emiyl.com>
2021-11-30 17:18:45 +0000

    added notes field and icons

--- docs/.vuepress/json/jailbreakFiles/chimera.js
+++ docs/.vuepress/json/jailbreakFiles/chimera.js
@@ -12,8 +12,10 @@ module.exports = {
         "url": "/installing-chimera/"
       }
     ],
+    "icon": "/assets/images/chimera.png"
+    "notes": "A12 support is only available on iOS 12.0 - 12.1.2.",
     "type": "Semi-untethered",
-    "firmwares": ["12.0","12.5.5"]
+    "firmwares": ["12.0","12.5.5"],
   },
   "compatibility": [
     {
