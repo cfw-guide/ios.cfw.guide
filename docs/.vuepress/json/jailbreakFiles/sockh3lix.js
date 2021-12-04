18ee4cd120a2c83043784baaf624434c6f189ec7
emiyl <me@emiyl.com>
2021-12-04 20:53:37 +0000

    fix

--- docs/.vuepress/json/jailbreakFiles/sockh3lix.js
+++ docs/.vuepress/json/jailbreakFiles/sockh3lix.js
@@ -71,6 +71,7 @@ module.exports = {
         "14G61", // 10.3.4
       ],
       "devices": [
+        "iPhone5,1", // iPhone 5 (GSM), A6
         "iPhone5,2", // iPhone 5 (CDMA), A6
       ]
     }
