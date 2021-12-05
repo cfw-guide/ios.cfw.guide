0f13ca4483002f4a5a766377233839e9d731b166
emiyl <me@emiyl.com>
2021-12-05 17:58:43 +0000

    firmware pages now will have iphoneos/tvos/apple tv software

--- docs/.vuepress/plugins/dynamicPages/lib/firmwareChart/index.js
+++ docs/.vuepress/plugins/dynamicPages/lib/firmwareChart/index.js
@@ -2,6 +2,7 @@ const iosList = require('../../../../json/ios');
 const fwPath = '/chart/firmware/'
 var page = [];
 
+// Find duplicates
 var iosVerArr = []
 var duplicateObjArr = []
 var duplicateVerArr = []
@@ -12,6 +13,28 @@ for (var i in iosList) {
 iosVerArr.forEach(function(x) { duplicateObjArr[x] = (duplicateObjArr[x] || 0) + 1; });
 for (var i in duplicateObjArr) if (duplicateObjArr[i] > 1) duplicateVerArr.push(i)
 
+function getOsStr(v) {
+  var ver = v.version;
+  var majFw = parseInt(ver.split('.')[0]);
+  
+  if (!v.hasOwnProperty('devices')) return 'iOS'
+  
+  var devArr = Object.entries(v.devices).map(function(x) { return x[0] });
+  
+  var hasiPhone = devArr.filter(function(x) { return x.startsWith('iPhone') }).length > 0
+  var hasiPad = devArr.filter(function(x) { return x.startsWith('iPad') }).length > 0
+  var hasiPod = devArr.filter(function(x) { return x.startsWith('iPod') }).length > 0
+  var hasTV = devArr.filter(function(x) { return x.startsWith('AppleTV') }).length > 0
+  
+  if (hasTV) {
+    if (majFw < 9) return 'Apple TV Software';
+    else return 'tvOS'
+  }
+  if (hasiPad && !hasiPhone && !hasiPod && majFw > 12) return 'iPadOS';
+  if (majFw < 4) return 'iPhoneOS';
+  return 'iOS';
+}
+
 for (var i in iosList) {
   const v = iosList[i];
   var redirects = '';
@@ -20,7 +43,7 @@ for (var i in iosList) {
   page.push({
     path: fwPath + v.build + '.html',
     frontmatter: {
-      title: 'iOS ' + v.version + ' (' + v.build + ')',
+      title: `${getOsStr(v)} ${v.version} (${v.build})`,
       description: 'Compatible jailbreaks for firmware version ' + v.version,
       redirect_from: redirects,
       sidebar: false,
