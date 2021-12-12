5ab9e7ea1e980c2b4fc225b4a333d1f54ee4a4bc
emiyl <me@emiyl.com>
2021-12-12 17:12:39 +0000

    moved template to theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,7 +81,4 @@ module.exports = {
       }
     }
   },
-  
-	templateDev: path.join(__dirname, 'templates', 'index.dev.html'),
-	templateSSR: path.join(__dirname, 'templates', 'index.ssr.html'),
 }
