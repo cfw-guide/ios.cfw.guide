fc169788dd3dc949bb31e96d2418c84b64267e7f
emiyl <me@emiyl.com>
2021-12-15 00:17:10 +0000

    made js file smaller

--- docs/.vuepress/plugins/redirectPlugin/lib/index.js
+++ docs/.vuepress/plugins/redirectPlugin/lib/index.js
@@ -2,7 +2,14 @@ const { path } = require('@vuepress/utils')
 
 module.exports = {
   onPrepared: async (app) => {
-    const pages = app.pages
+    var pages = app.pages
+    for (const i in pages) {
+      const t = pages[i];
+      pages[i] = {};
+      pages[i].path = t.path;
+      if (t.hasOwnProperty('frontmatter')) pages[i].frontmatter = {}; else continue;
+      if (t.frontmatter.hasOwnProperty('redirect_from')) pages[i].frontmatter.redirect_from = t.frontmatter.redirect_from; else continue;
+    }
     await app.writeTemp('pages.js', `export default ${JSON.stringify(pages)}`)
   },
   clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.ts'),
