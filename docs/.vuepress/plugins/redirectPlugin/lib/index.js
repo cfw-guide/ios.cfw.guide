b59884b9a1be42ea23c6ee3b8f830df29aa4751e
emiyl <me@emiyl.com>
2021-12-16 01:12:31 +0000

    fix

--- docs/.vuepress/plugins/redirectPlugin/lib/index.js
+++ docs/.vuepress/plugins/redirectPlugin/lib/index.js
@@ -2,15 +2,17 @@ const { path } = require('@vuepress/utils')
 
 module.exports = {
   onPrepared: async (app) => {
-    var pages = app.pages
+    const pages = app.pages
+    var retArr = [];
+
     for (const i in pages) {
-      const t = pages[i];
-      pages[i] = {};
-      pages[i].path = t.path;
-      if (t.hasOwnProperty('frontmatter')) pages[i].frontmatter = {}; else continue;
-      if (t.frontmatter.hasOwnProperty('redirect_from')) pages[i].frontmatter.redirect_from = t.frontmatter.redirect_from; else continue;
+      const page = pages[i];
+      var pageObj = {};
+      pageObj.path = page.path
+      if (page.hasOwnProperty('frontmatter')) if (page.frontmatter.hasOwnProperty('redirect_from')) pageObj.redirect_from = page.frontmatter.redirect_from; else continue; else continue;
+      retArr.push(pageObj);
     }
-    await app.writeTemp('pages.js', `export default ${JSON.stringify(pages)}`)
+    await app.writeTemp('pages.js', `export default ${JSON.stringify(retArr)}`)
   },
   clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.ts'),
 }
