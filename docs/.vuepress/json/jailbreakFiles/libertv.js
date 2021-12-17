d9594a7052259d88aab2af99aecfe24aad8a124c
emiyl <me@emiyl.com>
2021-12-17 11:53:03 +0000

    added ios 9 to 11

--- docs/.vuepress/json/jailbreakFiles/libertv.js
+++ docs/.vuepress/json/jailbreakFiles/libertv.js
@@ -12,5 +12,23 @@ module.exports = {
       external: true
     },
     latestVer: "1.1",
+    type: "Semi-untethered",
+    firmwares: ["11.0","11.1"],
   },
+  compatibility: [
+    {
+      firmwares: [
+        "14T330", // 10.0, AppleTV
+        "14U100", // 10.0.1, AppleTV
+        "14U72", // 10.0.1, AppleTV
+        "14U593", // 10.1, AppleTV
+        "15J381", // 11.0, AppleTV
+        "15J582", // 11.1, AppleTV
+      ],
+      devices: [
+        "AppleTV5,3", // Apple TV HD (4th generation), A8
+        "AppleTV6,2", // Apple TV 4K (1st generation), A10X
+      ]
+    }
+  ]
 }
