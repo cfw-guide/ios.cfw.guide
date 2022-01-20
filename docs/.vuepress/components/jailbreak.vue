eb46b41470bfb5b492dd6b17c3b276cc946305f4
emiyl <me@emiyl.com>
2022-01-20 21:10:36 +0000

    emma dumbass arc

--- docs/.vuepress/components/jailbreak.vue
+++ docs/.vuepress/components/jailbreak.vue
@@ -134,9 +134,9 @@ export default {
     },
     getDeviceList() {
       const compat = this.frontmatter.jailbreak.compatibility
+      if (!compat) return
       var devList = []
       for (var i in compat) {
-        if (!compat[i].hasOwnProperty('devices')) return
         for (var d in compat[i].devices) {
           const dev = compat[i].devices[d]
           var retg = json.groups.filter(g => g.devices.includes(dev))[0]
@@ -144,7 +144,6 @@ export default {
         }
       }
       devList = devList.map(function (x) {
-        if (!x.hasOwnProperty('devices')) return x
         x.firmwares = x.devices.map(function(d) {
           var ret = []
           const fwList = compat.filter(c => c.devices.includes(d)).map(f => f.firmwares)
@@ -162,7 +161,6 @@ export default {
         return x
       })
       devList = devList.map(function (x) {
-        if (!x.hasOwnProperty('devices')) return x
         x.devices = x.devices.map(d => json.device[d])
         if (x.hasOwnProperty('subgroups'))
           x.subgroups = x.subgroups.map(g => g.devices.map(d => json.device[d]))
@@ -174,7 +172,6 @@ export default {
       const compat = this.frontmatter.jailbreak.compatibility
       var devObj = {}
       this.getDeviceList.map(function(x) {
-        if (!x.hasOwnProperty('devices')) return x
         for (var d in x.devices) {
           const dev = x.devices[d].identifier
           devObj[dev] = {}
