de5cc27cecf449db152507d1020326e12b105a68
emiyl <me@emiyl.com>
2022-02-03 00:25:29 +0000

    made charts wider

--- docs/.vuepress/components/chartPage.vue
+++ docs/.vuepress/components/chartPage.vue
@@ -2,7 +2,7 @@
   <main class="page">
     <slot name="top" />
 
-    <div class="theme-default-content">
+    <div class="home">
       <h1>{{ pageTitle }}</h1>
       <div class="custom-container tip" v-if="discordNoticeText" v-html="discordNoticeText"/>
       
