f0f9d3979084a7463e95e7920e8034d3b38557fe
emiyl <me@emiyl.com>
2022-02-08 19:07:42 +0000

    replaced appledb links

--- docs/.vuepress/i18n/en_US/navbar.js
+++ docs/.vuepress/i18n/en_US/navbar.js
@@ -1,40 +1,5 @@
 module.exports = [
   {
-    text: 'Chart',
-    children: [
-      {
-        text: 'iPhone',
-        link: 'https://appledb.dev/device/iPhone.html',
-        hideExtern: true
-      },
-      {
-        text: 'iPad',
-        link: 'https://appledb.dev/device/iPad.html',
-        hideExtern: true
-      },
-      {
-        text: 'iPod',
-        link: 'https://appledb.dev/device/iPod.html',
-        hideExtern: true
-      },
-      {
-        text: 'Apple TV',
-        link: 'https://appledb.dev/device/AppleTV.html',
-        hideExtern: true
-      },
-      {
-        text: 'Apple Watch',
-        link: 'https://appledb.dev/device/AppleWatch.html',
-        hideExtern: true
-      },
-      {
-        text: 'All firmwares',
-        link: 'https://appledb.dev',
-        hideExtern: true
-      },
-    ]
-  },
-  {
     text: 'Guides',
     children: [
       {
@@ -97,4 +62,8 @@ module.exports = [
       },
     ],
   },
+  {
+    text: 'AppleDB',
+    link: 'https://appledb.dev/'
+  }
 ]
