176bcb4d0281acf92d7569a560bd28f1ca1a8e51
emiyl <me@emiyl.com>
2022-04-29 16:13:53 +0100

    removed unnecessary head

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,17 +31,6 @@ module.exports = {
 	],
   
   head: [
-    [ 'link', { rel: 'apple-touch-icon', sizes: "180x180", href: '/assets/favicon/apple-touch-icon.png' } ],
-    [ 'link', { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/assets/favicon/favicon-16x16.png' } ],
-    [ 'link', { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/assets/favicon/favicon-32x32.png' } ],
-    [ 'link', { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/assets/favicon/android-chrome-192x192.png' } ],
-    [ 'link', { rel: 'icon', type: 'image/png', sizes: "194x194", href: '/assets/favicon/favicon-194x194.png' } ],
-    [ 'link', { rel: 'mask-icon', color: '#2E3440', href: '/assets/favicon/safari-pinned-tab.svg' } ],
-    [ 'link', { rel: 'shortcut icon', href: '/assets/favicon/favicon.ico' } ],
-    [ 'meta', { name: 'msapplication-TileColor', content: '#2E3440' } ],
-    [ 'meta', { name: 'msapplication-TileImage', content: '/assets/favicon/mstile-144x144.png' } ],
-    [ 'meta', { name: 'msapplication-config', content: '/assets/favicon/browserconfig.xml' } ],
-    [ 'meta', { name: 'theme-color', content: '#2E3440' } ],
     [ 'link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' } ],
     ['script', {src: 'https://cdn.thisiswaldo.com/static/js/8531.js'}],
     ['script', {src: 'https://www.googletagmanager.com/gtag/js?id=UA-152619365-1'}],
